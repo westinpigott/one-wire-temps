c842c7807c20e8ca4606452acade7af2379abeeb
Westin Pigott <westinpigott@gmail.com>
2014-12-07 16:40:56 -0500

    Initial working version

--- OneWireTemps.js
+++ OneWireTemps.js
@@ -4,125 +4,132 @@ var util = require("util");
 
 // Constants
 var DEVICE_DS18B20 = 0x28;
+var UNIT_TYPE_RAW = 'RAW';
+var UNIT_TYPE_CELCIUS = 'CELCIUS';
+var UNIT_TYPE_FARENHEIT = 'FARENHEIT';
 
-// Internal functions
-function readSingle(owt, deviceNum) {
-	if(owt.isReady !== true) {
-		console.log('Not yet initialized.  Waiting 5ms.');
-		setTimeout(readSingle,5, owt, deviceNum);
-		return;
-	}
-	console.log('Reading Single',deviceNum);
+var OneWireTemps = function(board, pin){
 
-
-	if(!(deviceNum in owt.devices)) {
-		setTimeout(readTemperatures,0, owt);
-		return;
-	}		
-	owt.board.io.sendOneWireReset(owt.pin);
-
-	// tell the sensor we want the result and read it from the scratchpad
-	console.log('Device:',owt.devices[deviceNum]);
-	owt.board.io.sendOneWireWriteAndRead(owt.pin, owt.devices[deviceNum], 0xBE, 9, function(error, data) {
-		if(error) {
-			console.log('ERROR');
-		  console.log(error);
-		  //return;
-		}
-		if(data === null) {
-			console.log('Data is NULL for: ',deviceNum);
-			//setTimeout(readSingle,500,owt,deviceNum);
-			//return;
-		} else {
-			var raw = (data[1] << 8) | data[0];
-			//var celsius = raw / 16.0;
-			//var fahrenheit = celsius * 1.8 + 32.0;
-			
-			owt.temps[deviceNum] = raw;
-			//console.log(i);
-			//console.info(this.i, ' : ', fahrenheit);
-		}
-		setTimeout(readSingle,0,owt,deviceNum+1);
-	}.bind(owt,deviceNum));
+	// Setup properties
+	var _board = board;
+	var _pin = pin;
+	var _temps = [];
+	var _lastUpdate = [];
+	var _devices = [];
+	var _isReady = false;
 	
-}
+	// Setup the callbacks once it is initialized
+	_board.on("ready", function initialize() {
+		_board.io.sendOneWireConfig(_pin, true);
+		_board.io.sendOneWireSearch(_pin, function(error, allDevices) {
+			if(error) {
+				console.error(error);
+				return;
+			}
+			
+			// Clear temps
+			_temps = [];
+			_devices = [];
+			
+			// Parse out only the temperature probes
+			for (var i = 0, len = allDevices.length; i < len; i++) {
+				if(allDevices[i][0] == 0x28) {
+					_devices.push(allDevices[i]);
+					_temps.push(0);
+				}
+			}
+			_isReady = true;
+			// emit ready event
+			this.emit("ready");
+		}.bind(this));
+		
+		this.on("ready", function() {
+			readTemperatures(this);
+		});
+	}.bind(this));
 
-function readTemperatures(owt) {
-	console.log('Cycle Reset');
-	if(owt.isReady !== true) {
-		console.log('Not yet initialized.  Waiting 5ms.');
-		setTimeout(readTemperatures,5, owt);
-		return;
+	var readTemperatures = function readTemperatures() {
+		if(_isReady !== true) {
+			console.log('Not yet initialized.  Waiting 5ms.');
+			setTimeout(readTemperatures,5);
+			return;
+		}
+		for (var i = 0, len = _devices.length; i < len; i++) {
+			_board.io.sendOneWireReset(_pin);
+			_board.io.sendOneWireWrite(_pin, _devices[i], 0x44);
+		}
+		setTimeout(readSingle, 0, 0);
 	}
-	for (var i = 0, len = owt.devices.length; i < len; i++) {
-		owt.board.io.sendOneWireReset(owt.pin);
-		console.log('Convert Command: ',owt.devices[i], owt.pin);
-		owt.board.io.sendOneWireWrite(owt.pin, owt.devices[i], 0x44);
-	}
-	setTimeout(readSingle, 0, owt, 0);
-}
-
-function initialize(owt) {
-	owt.board.io.sendOneWireConfig(owt.pin, true);
-	owt.board.io.sendOneWireSearch(owt.pin, function(error, allDevices) {
-		if(error) {
-			console.error(error);
+	
+	var readSingle = function(deviceNum) {
+		if(_isReady !== true) {
+			console.log('Not yet initialized.  Waiting 5ms.');
+			setTimeout(readSingle,5, deviceNum);
 			return;
 		}
 		
-		// Clear temps
-		owt.temps = [];
-		owt.devices = [];
-		
-		// Parse out only the temperature probes
-		for (var i = 0, len = allDevices.length; i < len; i++) {
-			if(allDevices[i][0] == 0x28) {
-				owt.devices.push(allDevices[i]);
-				owt.temps.push(0);
+		if(!(deviceNum in _devices)) {
+			setTimeout(readTemperatures,0);
+			return;
+		}		
+		_board.io.sendOneWireReset(_pin);
+
+		// tell the sensor we want the result and read it from the scratchpad
+		_board.io.sendOneWireWriteAndRead(_pin, _devices[deviceNum], 0xBE, 9, function(error, data) {
+			if(error) {
+				console.log('ERROR');
+				console.log(error);
 			}
+			if(data === null) {
+				console.log('Data is NULL for: ',deviceNum);
+			} else {
+				_temps[deviceNum] = (data[1] << 8) | data[0];				
+			}
+			setTimeout(readSingle,0,deviceNum+1);
+		}.bind(deviceNum));
+		
+	}
+	
+	this.getTemperatures = function(unitType, callback) {
+		var temps = [];
+		_temps.forEach(function(entry, index) {
+			temps[index] = retreiveSingleTemp(unitType, index);
+		});
+		callback(temps);
+	}
+	
+	this.getSingleTemp = function(unitType, deviceNum, callback) {
+		
+		callback(retreiveSingleTemp(unitType, deviceNum));
+	}
+	
+	var retreiveSingleTemp = function(unitType, deviceNum) {
+		switch(unitType) {
+			case UNIT_TYPE_RAW:
+				return _temps[deviceNum];
+			case UNIT_TYPE_CELCIUS:
+				return convertToCelcius(_temps[deviceNum]);
+			case UNIT_TYPE_FARENHEIT:
+				return convertToFarenheit(_temps[deviceNum]);
+			default:
+				console.log('Invalid type: '+unitType.toString());
 		}
-		owt.isReady = true;
-		// emit ready event
-		owt.emit("ready");
-	}.bind(owt));
+	}
 	
-	owt.on("ready", function() {
-		readTemperatures(owt);
-	});
-}
-
-
-function OneWireTemps(board, pin) {
-	// Check board is correct type
+	var convertToCelcius = function(raw) {
+		return raw / 16.0;
+	}
 	
-	// Setup properties
-	this.board = board;
-	this.pin = pin;
-	this.temps = [];
-	this.lastUpdate = [];
-	this.devices = [];
-	this.isReady = false;
-	
-	// Setup the callbacks once it is initialized
-	board.on("ready", function () {
-        initialize(this);
-	}.bind(this));
-	
-	
+	var convertToFarenheit = function(raw) {
+		return convertToCelcius(raw) * 1.8 + 32;
+	}
+		
 }
 
 // Inherit event api
 util.inherits(OneWireTemps, Emitter);
 
-OneWireTemps.prototype.initializeDevices = function() {
-	initialize(this);
-}
-
-OneWireTemps.prototype.readSingleTemp = function(id, format) {
-	
-		//console.log(this.temps);
-	
-}
-
-module.exports = OneWireTemps;
-
+module.exports.obj = OneWireTemps;
+module.exports.unit_raw = UNIT_TYPE_RAW;
+module.exports.unit_celcius = UNIT_TYPE_CELCIUS;
+module.exports.unit_farenheit = UNIT_TYPE_FARENHEIT;
--- example.js
+++ example.js
@@ -1,13 +1,15 @@
 var five = require('johnny-five'), board
-var OneWireTemps = require('./OneWireTemps.js');
+var owts = require('./OneWireTemps.js');
 
 var pin = 3;
 var board = new five.Board();
 
-var owts = new OneWireTemps(board, pin);
+var onewiretemps = new owts.obj(board, pin);
 
-owts.on("ready", function() {
-	setInterval(owts.readSingleTemp,100, 0, 'eh');
+onewiretemps.on("ready", function() {
+	setInterval(onewiretemps.getTemperatures,100, owts.unit_farenheit, function(temps){
+		console.log(temps);
+	});
 });
 
 
